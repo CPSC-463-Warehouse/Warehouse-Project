5f0612a7830ebb79f9e32290f478806761fa927c
Justin Huang <justin_huang@csu.fullerton.edu>
2019-10-19 18:44:33 -0700

    added dynamic warehouse floor creator

---
+++ Warehouse/Assets/Script/CreatingWarehouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatingWarehouse : MonoBehaviour
+{
+    //change these to warehouse size variables x & z = size of square y = height of sqare
+    public float x = 30f;
+    public float y = 30f;
+    public float z = 25f;
+
+    // create bottom plane to new warehouse size 
+    void Start()
+    {
+        transform.localScale = new Vector3(x, y, z);
+    }
+}
