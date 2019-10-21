bf99293408bf9830593c2d318b4ae1b69f9c7683
Brian Bui <iambrianbui@yahoo.com>
2019-10-20 17:54:01 -0700

    SceneSwitcher done

---
+++ Warehouse/Assets/Script/SaveWarehouseInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveWarehouseInfo : MonoBehaviour
+{
+
+    public InputField inputText;
+    string sText;
+
+    void Start()
+    {
+        sText = PlayerPrefs.GetString("sTextName");
+        inputText.text = sText;
+    }
+
+
+    public void SaveThis()
+    {
+        sText = inputText.text;
+        PlayerPrefs.SetString("sTextName", sText);
+    }
+
+}
---
+++ Warehouse/Assets/Script/SceneSwitcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoToCreatingWarehouseScene()
+    {
+        SceneManager.LoadScene("CreatingWarehouse");
+    }
+
+    public void GoToMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void GoToListViewScene()
+    {
+        SceneManager.LoadScene("List View");
+    }
+
+    public void GoToViewWarehouseScene()
+    {
+        SceneManager.LoadScene("ViewWarehouse");
+    }
+
+    public void GoToNewItem()
+    {
+        SceneManager.LoadScene("NewItem");
+    }
+}
