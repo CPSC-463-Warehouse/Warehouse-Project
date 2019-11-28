e811b092233c963479f7ad4f89c4e5c38e996569
Justin Huang <justin_huang@csu.fullerton.ed>
2019-11-28 15:53:13 -0800

    asdf

--- Warehouse/Assets/Script/Wall.cs
+++ Warehouse/Assets/Script/Wall.cs
@@ -6,14 +6,17 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public GameObject block;
-    public int width = 50;
-    public int legnth = 50;
+    //public int width = 50;
+    //public int legnth = 50;
 
     void Start()
     {
+        int length = WarehouseInstance.instance.length;
+        int width = WarehouseInstance.instance.width;
+
         for (int x = 0; x < width; ++x)
         {
-            for (int y = 0; y < legnth; ++y)
+            for (int y = 0; y < length; ++y)
             {
                 Instantiate(block, new Vector3(x, 0, y), Quaternion.identity);
             }
